7817a57182b3a116c7962b209aac459d463f5f88
+ <+>
2022-09-17 10:38:07 +0100

    commit tree 4286771b661c518df10f04e70a0fcc8d4034a767

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name,email){
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name,
+            email,
+        }),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObj)
+        .then((resp) => resp.json())
+        .then(function(data) {
+            document.body.innerHTML = data.id;
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message;
+        })
+}
